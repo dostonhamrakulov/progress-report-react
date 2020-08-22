bc9377180749c0deae4df5548bd5e11d495f1018
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2020-08-22 22:20:08 +0200

    making reports component as main page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import './main.css';
 import About from './About';
-import Nav from './Nav';
 import Reports from './Reports';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
       <Router>
           <div className="App">
-              <Nav />
               <Switch>
-                  <Route path={'/'} exact component={Homepage} />
                   <Route path={'/about'} exact component={About} />
-                  <Route path={'/reports'} exact component={Reports} />
+                  <Route path={'/'} exact component={Reports} />
               </Switch>
 
           </div>
@@ -21,12 +18,5 @@ function App() {
 
   );
 }
-
-const Homepage = () => (
-    <div>
-        <h1>Homepage</h1>
-        <p>Please select what you want to see</p>
-    </div>
-);
 
 export default App;
--- src/report/Report.js
+++ src/report/Report.js
@@ -3,21 +3,22 @@ import './../main.css';
 import './report-timeline-page.css';
 import 'font-awesome/css/font-awesome.min.css';
 import ReportListSection from './ReportListSection';
+import { Link } from 'react-router-dom';
 
 export class Report extends React.Component {
     render() {
-
-        console.log("Report is prenting");
-        console.log(this.props.report);
-
         return (
             <div className="timeline__box">
                 <div className="timeline__date">
-                    <span className="timeline__day">2</span>
-                    <span className="timeline__month">AUG</span>
+                    <span className="timeline__day">{this.props.report.day}</span>
+                    <span className="timeline__month">{this.props.report.month}</span>
                 </div>
                 <div className="timeline__post">
-                    <div className="timeline__header">Week <span>20</span></div>
+                    <div className="timeline__header">
+                        <Link to={`/${this.props.report.week}`} className={'link'}>
+                            Week <span>{this.props.report.week}</span>
+                        </Link>
+                    </div>
                     <div className="timeline__content">
                         <h3> I have done in this week:</h3>
                         <ReportListSection sectionList={this.props.report.implemented}/>
@@ -28,13 +29,13 @@ export class Report extends React.Component {
                     </div>
                     <div className="timeline__footer">
                             <span className="timeline__footer_icon">
-                                <i className="fa fa-clock-o" aria-hidden="true"></i>
+                                <i className="fa fa-clock-o" aria-hidden="true"/>
                             </span>
                         <span>Created at {this.props.report.createdDate}</span>
                     </div>
                     <div className="timeline__footer">
                             <span className="timeline__footer_icon">
-                                <i className="fa fa-clock-o" aria-hidden="true"></i>
+                                <i className="fa fa-clock-o" aria-hidden="true"/>
                             </span>
                         <span>Updated at: {this.props.report.updateDate}</span>
                     </div>
